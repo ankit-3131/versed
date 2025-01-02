1a43a2a6995f4fb9c1cf52dcb786530a65c0b0ae
ankit-3131 <heyyynkit@gmail.com>
2025-01-02 22:06:33 +0530

    Add Note Functionality || User Specific Note

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,12 +1,15 @@
 const Note = require('../models/Notes');
 const mongoose = require('mongoose');
+
+//
 exports.dashboard = async (req, res)=>{
     if (!req.isAuthenticated()) {
         return res.redirect('/')
     }
+    // const result = await Note.deleteMany({ user: "67303af342cec86248155141" }); 
+    // const result = await Note.deleteMany({ user: "6728fe279856ffaacb1dcf2f" }); 
     async function insertDummyData(){
     try{
-      // const result = await Note.deleteMany({ user: "67303af342cec86248155141" }); 
         await Note.insertMany([
             {
                 user:"6728fe279856ffaacb1dcf2f",
@@ -26,7 +29,8 @@ exports.dashboard = async (req, res)=>{
         console.log(error);
     }
 }
-    insertDummyData();
+    // insertDummyData();
+
     const locals = {
         pageTitle: "Dashboard",
         description: "Dashboard page"
@@ -34,12 +38,18 @@ exports.dashboard = async (req, res)=>{
 
     // const notes = await Note.find({user: "6728fe279856ffaacb1dcf2f"}).sort({createdAt: -1}).
     try{
-      const notes = await Note.find({user:"67303af342cec86248155141"});
+      const notes = await Note.find({user: req.user}).sort({createdAt:-1});
       const user = req.user;
+      var flag = req.cookies?.flag || 100;
+      var content = req.cookies?.content || "Done!";
+      res.clearCookie('flag');
+      res.clearCookie('content');
     res.render('dashboard/index', {
       user,
       locals,
       notes,
+      flag,
+      content,
       layout: "../views/layouts/dashboard",
     });
     }
@@ -47,24 +57,45 @@ exports.dashboard = async (req, res)=>{
       console.log("Error", error);
     }
     };
+    //view note
 
     exports.dashboardViewNote = async(req,res)=>{
-      const note = await Note.findById({ _id: req.params.id }).where({user: req.user.id}).lean();
-
-      if(note){
-        res.render('dashboard/view-note', {
-          noteID: req.params.id,
-          note,
-          layout: '../views/layouts/dashboard'
-        })
-      }
+      try{
+          console.log("fetching note")
+          // const note = await Note.findById({ _id: req.params.id }).where({user: req.user.id}).lean();
+          const note = await Note.findById({ _id: req.params.id }).lean();
+          console.log("fetched note")
+          if(note){
+            res.render('dashboard/view-note', {
+              noteID: req.params.id,
+              note,
+              layout: '../views/layouts/dashboard'
+            })
+            console.log("served note")
+          }
+          else{
+            res.status(404).send("Note not found");
+          }
+        }
+        catch(error){
+          console.log("Error", error);
+        }
     }
+
+    //update note code
+
     exports.dashboardUpdateNote = async(req,res)=>{
       try{
         await Note.findOneAndUpdate(
           { _id: req.params.id},
           { title: req.body.title, body: req.body.body }
-        ).where( {user: req.user.id} );
+        );
+        res.cookie('flag', 1, { 
+          maxAge: 2000, 
+        });
+        res.cookie('content', "Updated the note Successfully!",  { 
+          maxAge: 2000, 
+        });
         res.redirect('/dashboard')
       } catch(error){
         console.log(error);
@@ -75,10 +106,45 @@ exports.dashboard = async (req, res)=>{
 
     exports.dashboardDeleteNote = async(req,res)=>{
       try{
-        await Note.deleteOne({ _id: req.params.id}).where({user: req.user.id});
-        res.redirect('/dashboard');
+        await Note.deleteOne({ _id: req.params.id});
+        res.cookie('flag', 1, { 
+          maxAge: 2000, 
+        });
+        res.cookie('content', "Deleted the note Successfully!",  { 
+          maxAge: 2000, 
+        });
+        return res.redirect('/dashboard');
       }
       catch(error){
         console.log(error);
+      }
+    }
+
+    //add note
+    
+    exports.dashboardAddNote =  async(req, res)=>{
+      try{
+        const title = req.body.title;
+        const body = req.body.body;
+        const userNow = req.user;
+
+        const addedNote = Note.create({
+          title: title,
+          body: body,
+          user: userNow,
+        })
+        const locals = {
+          pageTitle: "Add Note",
+          description: "Add a New Note"
+      }
+      res.cookie('flag', 1, { 
+        maxAge: 2000, 
+      });
+      res.cookie('content', "Added your new note!",  { 
+        maxAge: 2000, 
+      });
+      res.redirect('/dashboard')}
+      catch(error){
+        console.log(`error in creating the note`,error);
       }
     }
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,7 +4,16 @@ const dashboardController = require('../controllers/dashboardController');
 const {isLoggedIn} = require('../middleware/checkAuth')
 
 router.get('/dashboard',isLoggedIn, dashboardController.dashboard);
+// router.get('/dashboard', dashboardController.dashboard);
+// router.get('/dashboard/item/:id', dashboardController.dashboardViewNote);
 router.get('/dashboard/item/:id',isLoggedIn, dashboardController.dashboardViewNote);
+
+router.get('/dashboard/add',isLoggedIn, (req,res)=>{
+
+    res.render('dashboard/add-note', {layout: "../views/layouts/dashboard"});
+});
+
+router.post('/dashboard/add',isLoggedIn, dashboardController.dashboardAddNote);
 router.put('/dashboard/item/:id',isLoggedIn, dashboardController.dashboardUpdateNote);
 router.delete('/dashboard/item-delete/:id',isLoggedIn, dashboardController.dashboardDeleteNote);
 
